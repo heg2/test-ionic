d51776943a6865577b33f5ec12111219b50fd063
Gabriel Hess <heg2@bfh.ch>
2020-09-25 10:20:11 +0200

    test login use old version of jssha as workaround for bug

--- src/app/explore-container/explore-container.component.ts
+++ src/app/explore-container/explore-container.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 
+import { IonicOnFhirService } from '@i4mi/ionic-on-fhir';
+
 @Component({
   selector: 'app-explore-container',
   templateUrl: './explore-container.component.html',
@@ -8,8 +10,22 @@ import { Component, OnInit, Input } from '@angular/core';
 export class ExploreContainerComponent implements OnInit {
   @Input() name: string;
 
-  constructor() { }
+  constructor(private ionicOnFhir: IonicOnFhirService) {
 
-  ngOnInit() {}
+  }
+
+  ngOnInit() {
+      this.ionicOnFhir.initIonicOnFhir('https://test.midata.coop', 'midemodev');
+  }
+
+  login() {
+      console.log('clicked login');
+      this.ionicOnFhir.authenticate().then((res) => {
+          console.log('auth successful', res)
+      })
+      .catch((err) => {
+          console.log('auth failed', err)
+      })
+  }
 
 }
